8c585a68e4e1c1f30efbfa1edc11f91d2aa5bb52
sinfovi <sinfovi@gmail.com>
2015-11-18 11:39:44 -0300

    Correcao1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 	
 	before_action :configure_permitted_parameters, if: :devise_controller?
 	def configure_permitted_parameters
-		devise_parameter_sanitizer.for(:sign_up)  { |u| u.permit(:name,  :cpf, 
+			devise_parameter_sanitizer.for(:sign_up)  { |u| u.permit(:name,  :cpf, 
 			 :city, :telephone, :institution ,:email,:password, 
 			 :password_confirmation,:birthday, :state, :shirt,
 			 registereds_attributes: [:id, :user_id, :course_id])}
